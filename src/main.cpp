a83709dfcc168d1a38f7f5f2177ad6fdd55a171c
Avdiji <favdiji@googlemail.com>
2024-03-28 19:44:50 +0100

    initial setup for OpenGL Projects

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <GLFW/glfw3.h>
+#include <main.hpp>
+
+int main() {
+    /* Initialize the library */
+    if (!glfwInit()) { return -1; }
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow *window = glfwCreateWindow(1920, 1080, "GLFW CMake starter", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glClearColor(1, 1, 1, 0.0f);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
